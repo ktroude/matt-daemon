b321856f19cea6c750e88d9fb38835d0b852b224
Kevin Troude <ktroude@c3r2p4.42nice.fr>
2025-05-15 16:42:36 +0200

    test phase

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
-#include "../include/TintinReporter.hpp"
+#include "../include/Daemon.hpp"
 
 int main() {
-
+    try {
+        Daemon daemon;
+        return daemon.run();
+    } catch (const std::exception& e) {
+        return 1;
+    }
 }
